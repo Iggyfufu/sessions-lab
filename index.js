41701b28082366cdda48456fa43b62ada9825d74
Iggyfufu <kennethzhu07@gmail.com>
2018-05-21 14:43:46 -0400

    commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const session = require('express-session')
+
+const app = express()
+
+
+app.use(session({
+  secret: 'SunnyB3aches',
+  resave: false,
+  saveUninitialized: true,
+}))
+
+
+app.use((req, res, next) => {
+  if (!req.session.counter) req.session.counter = 0
+  console.log('counter', ++req.session.counter)
+  next()
+})
+
+app.use((req, res, next) => {
+  console.log('SESSION: ', req.session)
+  next()
+})
+
+app.get('/', (req, res) => {
+  res.send('hello')
+})
+
+app.listen(8080, () => console.log('Listening at http://localhost:8080'))
